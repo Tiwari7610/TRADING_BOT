674b89e54fbb30534fe108633facd623312cd9d5
Priyanshu Tiwari <tpriyanshu095@gmail.com>
2026-03-14 10:56:42 +0530

    Updated Binance Futures Trading Bot with logging and CLI

--- bot/client.py
+++ bot/client.py
@@ -1,11 +1,13 @@
 from binance.client import Client
 
-API_KEY = "tUG8tdF3ay4DILj9Kpmll2nizDM4Qf5mL0Kalwd4oJQ7vJlJT4IiYALN6HuF5wic"
-API_SECRET = "gDT6LKbBTc0ShquTWHvuTHWAZD2zD5ONn029BwNF06gk5Pj3sCqad41sndUUc69A"
-
-BASE_URL = "https://testnet.binancefuture.com"
+API_KEY = "LsnmsVqxvjSVoOAYeoHsRhnGzBrdkCPlSQhbDhutmPqKxt4UcuLwtYnSQcLmdC6U"
+API_SECRET = "JIvMORkHMIpZi4oVFhdxtpe5QNYgqG6kPNc0qThzabpkHe94QkKIIUVL5knThdYk"
 
 def get_client():
+
     client = Client(API_KEY, API_SECRET)
-    client.FUTURES_URL = BASE_URL
+
+    # Correct Binance Futures Testnet URL
+    client.FUTURES_URL = "https://testnet.binancefuture.com/fapi"
+
     return client
